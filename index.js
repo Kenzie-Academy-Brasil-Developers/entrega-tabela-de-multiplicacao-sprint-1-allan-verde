14e51e2bb91e49adb43fda5e06ea28e708e9f1e9
Allan Verde <allangreen2906@gmail.com>
2021-08-12 14:38:52 -0300

    Criando arquivos base

---
+++ index.js
@@ -0,0 +1,9 @@
+// Se preferir, utilize o script em um arquivo .js separado.
+function nomeDaSuaFuncao() { // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
+    // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
+    let x = [];
+    for (let i = 0; i <= 10; i++) {
+        x[i] = [];
+    }
+    console.table(x)
+}
